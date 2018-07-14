1f597a89f81c54e1ec899d62f64e78e666ab1d92
Jevade <liujiawei0524@gmail.com>
2018-07-14 10:45:12 +0800

    auto push at 2018-07-14 10:45:12.144078

--- upload_crontab.py
+++ upload_crontab.py
@@ -4,9 +4,7 @@ import os
 import subprocess
 import datetime
 filepath = os.path.dirname(__file__)
-print(filepath)
-subprocess.call(["ls"])
-subprocess.call(["cd",filepath])
+os.chdir(filepath)
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
 subprocess.call(["git", "push"])
